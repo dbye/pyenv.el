053a6fe4cec24707dfc961c4ec1edacc48b58809
Daniel Bye <dan@slightlystrange.org>
2015-01-27 14:25:53 +0000

    Converted rbenv.el to pyenv.pl

--- pyenv.el
+++ pyenv.el
@@ -1,12 +1,12 @@
-;;; rbenv.el --- Emacs integration for rbenv
+;;; pyenv.el --- Emacs integration for pyenv
 
 ;; Copyright (C) 2013 Yves Senn
 
-;; URL: https://github.com/senny/rbenv.el
+;; URL: https://github.com/senny/pyenv.el
 ;; Author: Yves Senn <yves.senn@gmail.com>
 ;; Version: 0.0.3
 ;; Created: 10 February 2013
-;; Keywords: python rbenv
+;; Keywords: python pyenv
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -29,183 +29,183 @@
 
 ;;; Commentary:
 
-;; M-x global-rbenv-mode toggle the configuration done by rbenv.el
+;; M-x global-pyenv-mode toggle the configuration done by pyenv.el
 
-;; M-x rbenv-use-global prepares the current Emacs session to use
-;; the global python configured with rbenv.
+;; M-x pyenv-use-global prepares the current Emacs session to use
+;; the global python configured with pyenv.
 
-;; M-x rbenv-use allows you to switch the current session to the python
+;; M-x pyenv-use allows you to switch the current session to the python
 ;; implementation of your choice.
 
 ;;; Compiler support:
 
 ;; helper function used in variable definitions
-(defcustom rbenv-installation-dir (or (getenv "RBENV_ROOT")
-                                      (concat (getenv "HOME") "/.rbenv/"))
-  "The path to the directory where rbenv was installed."
-  :group 'rbenv
+(defcustom pyenv-installation-dir (or (getenv "PYENV_ROOT")
+                                      (concat (getenv "HOME") "/.pyenv/"))
+  "The path to the directory where pyenv was installed."
+  :group 'pyenv
   :type 'directory)
 
-(defun rbenv--expand-path (&rest segments)
+(defun pyenv--expand-path (&rest segments)
   (let ((path (mapconcat 'identity segments "/"))
-        (installation-dir (replace-regexp-in-string "/$" "" rbenv-installation-dir)))
+        (installation-dir (replace-regexp-in-string "/$" "" pyenv-installation-dir)))
     (expand-file-name (concat installation-dir "/" path))))
 
-(defcustom rbenv-interactive-completion-function
+(defcustom pyenv-interactive-completion-function
   (if ido-mode 'ido-completing-read 'completing-read)
-  "The function which is used by rbenv.el to interactivly complete user input"
-  :group 'rbenv
+  "The function which is used by pyenv.el to interactivly complete user input"
+  :group 'pyenv
   :type 'function)
 
-(defcustom rbenv-show-active-python-in-modeline t
-  "Toggles wether rbenv-mode shows the active python in the modeline."
-  :group 'rbenv
+(defcustom pyenv-show-active-python-in-modeline t
+  "Toggles wether pyenv-mode shows the active python in the modeline."
+  :group 'pyenv
   :type 'boolean)
 
-(defcustom rbenv-modeline-function 'rbenv--modeline-with-face
-  "Function to specify the rbenv representation in the modeline."
-  :group 'rbenv
+(defcustom pyenv-modeline-function 'pyenv--modeline-with-face
+  "Function to specify the pyenv representation in the modeline."
+  :group 'pyenv
   :type 'function)
 
-(defvar rbenv-executable (rbenv--expand-path "bin" "rbenv")
-  "path to the rbenv executable")
+(defvar pyenv-executable (pyenv--expand-path "bin" "pyenv")
+  "path to the pyenv executable")
   
-(defvar rbenv-python-shim (rbenv--expand-path "shims" "python")
+(defvar pyenv-python-shim (pyenv--expand-path "shims" "python")
   "path to the python shim executable")
 
-(defvar rbenv-global-version-file (rbenv--expand-path "version")
-  "path to the global version configuration file of rbenv")
+(defvar pyenv-global-version-file (pyenv--expand-path "version")
+  "path to the global version configuration file of pyenv")
 
-(defvar rbenv-version-environment-variable "RBENV_VERSION"
-  "name of the environment variable to configure the rbenv version")
+(defvar pyenv-version-environment-variable "PYENV_VERSION"
+  "name of the environment variable to configure the pyenv version")
 
-(defvar rbenv-binary-paths (list (cons 'shims-path (rbenv--expand-path "shims"))
-                                 (cons 'bin-path (rbenv--expand-path "bin")))
-  "these are added to PATH and exec-path when rbenv is setup")
+(defvar pyenv-binary-paths (list (cons 'shims-path (pyenv--expand-path "shims"))
+                                 (cons 'bin-path (pyenv--expand-path "bin")))
+  "these are added to PATH and exec-path when pyenv is setup")
 
-(defface rbenv-active-python-face
+(defface pyenv-active-python-face
   '((t (:weight bold :foreground "Red")))
   "The face used to highlight the current python on the modeline.")
 
-(defvar rbenv--initialized nil
-  "indicates if the current Emacs session has been configured to use rbenv")
+(defvar pyenv--initialized nil
+  "indicates if the current Emacs session has been configured to use pyenv")
 
-(defvar rbenv--modestring nil
-  "text rbenv-mode will display in the modeline.")
-(put 'rbenv--modestring 'risky-local-variable t)
+(defvar pyenv--modestring nil
+  "text pyenv-mode will display in the modeline.")
+(put 'pyenv--modestring 'risky-local-variable t)
 
 ;;;###autoload
-(defun rbenv-use-global ()
-  "activate rbenv global python"
+(defun pyenv-use-global ()
+  "activate pyenv global python"
   (interactive)
-  (rbenv-use (rbenv--global-python-version)))
+  (pyenv-use (pyenv--global-python-version)))
 
 ;;;###autoload
-(defun rbenv-use-corresponding ()
+(defun pyenv-use-corresponding ()
   "search for .python-version and activate the corresponding python"
   (interactive)
-  (let ((version-file-path (or (rbenv--locate-file ".python-version")
-                               (rbenv--locate-file ".rbenv-version"))))
-    (if version-file-path (rbenv-use (rbenv--read-version-from-file version-file-path))
-      (message "[rbenv] could not locate .python-version or .rbenv-version"))))
+  (let ((version-file-path (or (pyenv--locate-file ".python-version")
+                               (pyenv--locate-file ".pyenv-version"))))
+    (if version-file-path (pyenv-use (pyenv--read-version-from-file version-file-path))
+      (message "[pyenv] could not locate .python-version or .pyenv-version"))))
 
 ;;;###autoload
-(defun rbenv-use (python-version)
+(defun pyenv-use (python-version)
   "choose what python you want to activate"
   (interactive
-   (let ((picked-python (rbenv--completing-read "Ruby version: " (rbenv/list))))
+   (let ((picked-python (pyenv--completing-read "Ruby version: " (pyenv/list))))
      (list picked-python)))
-  (rbenv--activate python-version)
-  (message (concat "[rbenv] using " python-version)))
+  (pyenv--activate python-version)
+  (message (concat "[pyenv] using " python-version)))
 
-(defun rbenv/list ()
+(defun pyenv/list ()
   (append '("system")
-          (split-string (rbenv--call-process "versions" "--bare") "\n")))
+          (split-string (pyenv--call-process "versions" "--bare") "\n")))
 
-(defun rbenv--setup ()
-  (when (not rbenv--initialized)
-    (dolist (path-config rbenv-binary-paths)
+(defun pyenv--setup ()
+  (when (not pyenv--initialized)
+    (dolist (path-config pyenv-binary-paths)
       (let ((bin-path (cdr path-config)))
         (setenv "PATH" (concat bin-path ":" (getenv "PATH")))
         (add-to-list 'exec-path bin-path)))
     (setq eshell-path-env (getenv "PATH"))
-    (setq rbenv--initialized t)
-    (rbenv--update-mode-line)))
+    (setq pyenv--initialized t)
+    (pyenv--update-mode-line)))
 
-(defun rbenv--teardown ()
-  (when rbenv--initialized
-    (dolist (path-config rbenv-binary-paths)
+(defun pyenv--teardown ()
+  (when pyenv--initialized
+    (dolist (path-config pyenv-binary-paths)
       (let ((bin-path (cdr path-config)))
         (setenv "PATH" (replace-regexp-in-string (regexp-quote (concat bin-path ":")) "" (getenv "PATH")))
         (setq exec-path (remove bin-path exec-path))))
     (setq eshell-path-env (getenv "PATH"))
-    (setq rbenv--initialized nil)))
+    (setq pyenv--initialized nil)))
 
-(defun rbenv--activate (python-version)
-  (setenv rbenv-version-environment-variable python-version)
-  (rbenv--update-mode-line))
+(defun pyenv--activate (python-version)
+  (setenv pyenv-version-environment-variable python-version)
+  (pyenv--update-mode-line))
 
-(defun rbenv--completing-read (prompt options)
-  (funcall rbenv-interactive-completion-function prompt options))
+(defun pyenv--completing-read (prompt options)
+  (funcall pyenv-interactive-completion-function prompt options))
 
-(defun rbenv--global-python-version ()
-  (if (file-exists-p rbenv-global-version-file)
-      (rbenv--read-version-from-file rbenv-global-version-file)
+(defun pyenv--global-python-version ()
+  (if (file-exists-p pyenv-global-version-file)
+      (pyenv--read-version-from-file pyenv-global-version-file)
     "system"))
 
-(defun rbenv--read-version-from-file (path)
+(defun pyenv--read-version-from-file (path)
   (with-temp-buffer
     (insert-file-contents path)
-    (rbenv--replace-trailing-whitespace (buffer-substring-no-properties (point-min) (point-max)))))
+    (pyenv--replace-trailing-whitespace (buffer-substring-no-properties (point-min) (point-max)))))
 
-(defun rbenv--locate-file (file-name)
+(defun pyenv--locate-file (file-name)
   "searches the directory tree for an given file. Returns nil if the file was not found."
   (let ((directory (locate-dominating-file default-directory file-name)))
     (when directory (concat directory file-name))))
 
-(defun rbenv--call-process (&rest args)
+(defun pyenv--call-process (&rest args)
   (with-temp-buffer
-    (let* ((success (apply 'call-process rbenv-executable nil t nil
+    (let* ((success (apply 'call-process pyenv-executable nil t nil
                            (delete nil args)))
            (raw-output (buffer-substring-no-properties
                         (point-min) (point-max)))
-           (output (rbenv--replace-trailing-whitespace raw-output)))
+           (output (pyenv--replace-trailing-whitespace raw-output)))
       (if (= 0 success)
           output
         (message output)))))
 
-(defun rbenv--replace-trailing-whitespace (text)
+(defun pyenv--replace-trailing-whitespace (text)
   (replace-regexp-in-string "[[:space:]\n]+\\'" "" text))
 
-(defun rbenv--update-mode-line ()
-  (setq rbenv--modestring (funcall rbenv-modeline-function
-                                   (rbenv--active-python-version))))
+(defun pyenv--update-mode-line ()
+  (setq pyenv--modestring (funcall pyenv-modeline-function
+                                   (pyenv--active-python-version))))
 
-(defun rbenv--modeline-with-face (current-python)
+(defun pyenv--modeline-with-face (current-python)
   (append '(" [")
-          (list (propertize current-python 'face 'rbenv-active-python-face))
+          (list (propertize current-python 'face 'pyenv-active-python-face))
           '("]")))
 
-(defun rbenv--modeline-plain (current-python)
+(defun pyenv--modeline-plain (current-python)
   (list " [" current-python "]"))
 
-(defun rbenv--active-python-version ()
-  (or (getenv rbenv-version-environment-variable) (rbenv--global-python-version)))
+(defun pyenv--active-python-version ()
+  (or (getenv pyenv-version-environment-variable) (pyenv--global-python-version)))
 
 ;;;###autoload
-(define-minor-mode global-rbenv-mode
-  "use rbenv to configure the python version used by your Emacs."
+(define-minor-mode global-pyenv-mode
+  "use pyenv to configure the python version used by your Emacs."
   :global t
-  (if global-rbenv-mode
+  (if global-pyenv-mode
       (progn
-        (when rbenv-show-active-python-in-modeline
-          (unless (memq 'rbenv--modestring global-mode-string)
+        (when pyenv-show-active-python-in-modeline
+          (unless (memq 'pyenv--modestring global-mode-string)
             (setq global-mode-string (append (or global-mode-string '(""))
-                                             '(rbenv--modestring)))))
-        (rbenv--setup))
-    (setq global-mode-string (delq 'rbenv--modestring global-mode-string))
-    (rbenv--teardown)))
+                                             '(pyenv--modestring)))))
+        (pyenv--setup))
+    (setq global-mode-string (delq 'pyenv--modestring global-mode-string))
+    (pyenv--teardown)))
 
-(provide 'rbenv)
+(provide 'pyenv)
 
-;;; rbenv.el ends here
+;;; pyenv.el ends here
